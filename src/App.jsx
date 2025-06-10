04ca4b0a11c9924f4f1adda5e8bfc5c42eff42ee
Gregory Subero <suberog010@gmail.com>
2025-06-10 19:35:22 +0000

    Arreglando el useState del componente input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Counter} from './components/Counter';
-import { ToggleButton } from './components/ToggleButton';
-import { NameFor } from './components/NameFor';
+import {ToggleButton} from './components/ToggleButton';
+import {NameFor} from './components/NameFor';
 import './App.css'
 
 function App() {
--- src/components/NameFor.jsx
+++ src/components/NameFor.jsx
@@ -2,14 +2,19 @@ import { useState } from "react"
 
 
 export const NameFor = () => {
-    const {name, setName} = useState("");
+    const [name, setName] = useState("");
 
     return (
         <div>
-            <input type="text" placeholder="Ingresa tu Nombre" value={name} onChange={(event) => setName(event.target.value)} />
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
 
-            <p>Hola {name || "visitante"}</p>
+            <input type="text"
+             placeholder="Ingresa tu Nombre"
+             value={name}
+             onChange={(event) => setName(event.target.value)}
+             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+            <p className="dark:text-white font-medium text-sm">Hola, {name || "Visitante"}</p>
         </div>
-        
     )
 }
